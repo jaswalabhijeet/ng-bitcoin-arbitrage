d5a216e79850bc5b52128a78365b35c5c678e985
Sjors Provoost <sjors@sprovoost.nl>
2014-02-22 14:34:50 -0800

    Fetch values from server.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,44 +1,8 @@
 'use strict';
 
 angular.module('ngBitcoinArbitrageApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.opportunities = 
-    [
-        {
-          "buyprice": 256.16576, 
-          "kask": "MtGoxUSD", 
-          "kbid": "BitstampUSD", 
-          "perc": 144.57246027604288, 
-          "profit": 3703.4514161682328, 
-          "sellprice": 256.6, 
-          "volume": 10.0
-        }, 
-        {
-          "buyprice": 256.16576, 
-          "kask": "MtGoxUSD", 
-          "kbid": "BitstampUSD", 
-          "perc": 144.57246027604288, 
-          "profit": 3703.4514161682328, 
-          "sellprice": 256.6, 
-          "volume": 10.0
-        }, 
-        {
-          "buyprice": 256.16576, 
-          "kask": "MtGoxUSD", 
-          "kbid": "CampBXUSD", 
-          "perc": 137.24078881212426, 
-          "profit": 3515.639096905731, 
-          "sellprice": 256.56, 
-          "volume": 10.0
-        }, 
-        {
-          "buyprice": 256.16576, 
-          "kask": "MtGoxUSD", 
-          "kbid": "CampBXUSD", 
-          "perc": 137.24078881212426, 
-          "profit": 3515.639096905731, 
-          "sellprice": 256.56, 
-          "volume": 10.0
-        }
-      ]
+  .controller('MainCtrl', function ($scope, $http) {
+    $http.get('http://192.168.1.124:5000/bitcoin-arbitrage/api/v1.0/opportunities').success(function(data) {
+      $scope.opportunities = data.opportunities;
+    });
   });
