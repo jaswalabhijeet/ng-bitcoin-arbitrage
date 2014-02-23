d6a09ace4b5b4dad9108adb46f94a603476d9415
Luís Martinho <lmartinho@gmail.com>
2014-02-22 21:31:06 -0800

    moved update function to inside controller, now getting deps via closure

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,41 +1,39 @@
 'use strict';
 
-
-function update ($scope, $http) {
-  $http.get('http://localhost:5000/bitcoin-arbitrage/api/v1.0/opportunities').success(function(data) {
-    $scope.opportunities = data.opportunities;
-    $scope.chartObject = {};
-
-    var rows = [];
-
-    for(var oppIdx in $scope.opportunities) {
-      var opp = $scope.opportunities[oppIdx];
-      rows.push(
-        {c: [
-          {v: (opp.kbid ) + ' / ' + (opp.kask)},
-          {v: opp.profit },
-          {v: numeral(opp.profit).format('$0.00') }
-        ]}
-      );
-    }
-    
-    $scope.chartObject.data = {'cols': [
-        {id: 't', label: 'Topping', type: 'string'},
-        {id: 's', label: 'Profit ', type: 'number', prefix:'$', p: {}},
-      ], 'rows': rows};
-
-    $scope.chartObject.type = 'BarChart';
-    $scope.chartObject.options = {
-      'title': 'Profit by arbitrage opportunity'
-    };
-  });
-}
-
 angular.module('ngBitcoinArbitrageApp')
   .controller('MainCtrl', function ($scope, $http, $log) {
-    update($scope, $http);
-    setInterval(function() {
-      update($scope, $http);
-    }, 10000);
+
+    function update () {
+      $http.get('http://localhost:5000/bitcoin-arbitrage/api/v1.0/opportunities').success(function(data) {
+        $scope.opportunities = data.opportunities;
+        $scope.chartObject = {};
+
+        var rows = [];
+
+        for(var oppIdx in $scope.opportunities) {
+          var opp = $scope.opportunities[oppIdx];
+          rows.push(
+            {c: [
+              {v: (opp.kbid ) + ' / ' + (opp.kask)},
+              {v: opp.profit },
+              {v: numeral(opp.profit).format('$0.00') }
+            ]}
+          );
+        }
+
+        $scope.chartObject.data = {'cols': [
+          {id: 't', label: 'Topping', type: 'string'},
+          {id: 's', label: 'Profit ', type: 'number', prefix:'$', p: {}},
+        ], 'rows': rows};
+
+        $scope.chartObject.type = 'BarChart';
+        $scope.chartObject.options = {
+          'title': 'Profit by arbitrage opportunity'
+        };
+      });
+    }
+
+    update();
+    setInterval(update, 10000);
   });
   
